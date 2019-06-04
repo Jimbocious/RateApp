efd0832ff847cd5211363a4b77240b70120ea613
jimmy <jimsher.sinjaradze.1@btu.edu.ge>
2019-06-05 01:31:02 +0400

    fix directory links with node js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 80;
+
+// app.use(express.static('images'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));

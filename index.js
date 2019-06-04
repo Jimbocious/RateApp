462f31a3e5f2fe25aabbfc6f90fa731a364143b1
Hammerten <gvalishvili@gmail.com>
2019-06-05 00:14:50 +0400

    add html render

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-let app = require("express")();
+const app = require("express")();
+const path = require("path");
+const PORT = process.env.PORT || 80;
 
-app.get("/",(req,res) => {
-    res.send ("<h1>Main page</h1>");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.listen(80);
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname + "/about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname + "/contact.html"));
+});
+
+app.get("*", (req, res) => {
+  res.send("404");
+});
+
+app.listen(PORT, () => {
+  console.log(`Running at port ${PORT}`);
+});
